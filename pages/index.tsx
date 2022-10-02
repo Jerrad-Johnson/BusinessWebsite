ba12dc1668304da89179d9584ce258bed4c973fe
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-10-02 14:54:12 -0600

    Project Setup in Progress.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+function Home<NextPage>(){
+
   return (
     <div className={styles.container}>
       <Head>
