98195368120d833735c127e08d09ea69281c7bcb
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-06 13:06:04 -0700

    Hash and compare fns  operate correctly.

--- backend/controllers/admin/AdminLoginControllerPost.js
+++ backend/controllers/admin/AdminLoginControllerPost.js
@@ -7,8 +7,6 @@ const bcrypt = require('../../common/custom-bcrypt');
 exports.AdminLoginController = async (req, res, next) => {
     let query = "SELECT password FROM admin_account WHERE username = ?";
     let queryResult = await genericSQLPromise(query, [req.body.username], res);
-    let hashed = bcrypt.hash(queryResult.data[0].password)
-    cc(hashed);
-    //cc(queryResult);
-    res.status(200).send(standardizedResponse(hashed));
+    let comparison = bcrypt.compare(req.body.password, queryResult.data[0].password);
+    //res.status(200).send(standardizedResponse(hashed));
 }
