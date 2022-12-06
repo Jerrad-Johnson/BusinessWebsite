f0f38eb0a7d707dbf019a7d0eb68dac95896bf4e
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-06 12:52:12 -0700

    Added dependency md5.

--- backend/controllers/admin/AdminLoginControllerPost.js
+++ backend/controllers/admin/AdminLoginControllerPost.js
@@ -1,10 +1,13 @@
 const {standardizedResponse} = require("../../utils/fns");
 const {genericSQLPromise} = require("../../common/queries");
 
+
+
 exports.AdminLoginController = async (req, res, next) => {
-    let query = "SELECT username FROM admin_account WHERE username = ? AND password = ?";
-    cc(req.body.password);
-    let queryResult = genericSQLPromise(query, [req.body.username, req.body.password], res);
+    let query = "SELECT password FROM admin_account WHERE username = ?";
+    let queryResult = await genericSQLPromise(query, [req.body.username], res);
+
+    cc(queryResult);
     //cc(queryResult);
     //res.status(200).send(standardizedResponse("test", {something: "sommewhere"}));
 }
