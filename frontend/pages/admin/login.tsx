036c2e24c4a531478179b439fd121669dc88139a
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-05 22:16:52 -0700

    Added dependency Axios.

--- frontend/pages/admin/login.tsx
+++ frontend/pages/admin/login.tsx
@@ -1,10 +1,18 @@
 import type {JSXElement} from "@babel/types";
 import useSWR from "swr";
+
 const cc = console.log;
 
 const loginPanel: JSX.Element = (
   <div className={"container container__login container--login--admin"}>
-      <h1>Admin Login</h1>
+      <h1 className={"container__main-header"}>Admin Login</h1>
+      <form>
+          <input type={"text"} placeholder={"Username"}/>
+          <input type={"password"} placeholder={"Password"}/>
+          <button className={"button button-submit"} onClick={(e) => {
+              e.preventDefault();
+          }}>Submit</button>
+      </form>
   </div>
 );
 
@@ -16,8 +24,8 @@ function Login<NextPage>(): JSX.Element{
     );
 }
 
-async function updateImages(){
-    //let result = await
+async function handleLogin(): Promise<void>{
+
 }
 
 
