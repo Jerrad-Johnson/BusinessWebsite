2494b7e442ad23f8dc7e89b519785db5763640eb
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-07 00:53:26 -0700

    Now gets the exif data from each image, in all folders (for leaflet).

--- backend/models/admin/images/getExifForMapUpdate.js
+++ backend/models/admin/images/getExifForMapUpdate.js
@@ -2,17 +2,16 @@ const ExifReader = require('exifreader');
 
 exports.getExifForMapUpdate = async (fileAndFolderNames) => {
     let entry;
-    let exifResults = [];
+    let exifResults = {};
     const imagePath = `./public/map_images`
 
     for (let folderName in fileAndFolderNames){
+        exifResults[folderName] = [];
         for (let fileName of fileAndFolderNames[folderName]){
-            cc(`${imagePath}/${folderName}/${fileName}`);
+            entry = await ExifReader.load(`${imagePath}/${folderName}/${fileName}`);
+            entry.fileName = fileName;
+            exifResults[folderName].push(entry);
         }
-        /*entry = await ExifReader.load(`${imagePath}/${folderName}/${fileName}`);
-        entry.fileName = fileName;
-        exifResults.push(entry);*/
     }
-
-    //return exifResults;
+    return exifResults;
 }
