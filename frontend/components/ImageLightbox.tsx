1af9ed47e49b9137cb732eaa3e222e53adffe35e
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-22 00:02:36 -0700

    Added package react-grid-gallery

--- frontend/components/ImageLightbox.tsx
+++ frontend/components/ImageLightbox.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Lightbox from 'react-18-image-lightbox';
 import 'react-18-image-lightbox/style.css'; // This only needs to be imported once in your app
+import { Gallery } from "react-grid-gallery";
 
 const images = [
     '//placekitten.com/1500/500',
@@ -21,10 +22,40 @@ export default class ImageLightbox extends Component {
 
     render() {
         const { photoIndex, isOpen } = this.state;
+        const images = [
+            {
+                src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+                width: 320,
+                height: 174,
+                isSelected: true,
+                caption: "After Rain (Jeshu John - designerspics.com)",
+            },
+            {
+                src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+                width: 320,
+                height: 212,
+                tags: [
+                    { value: "Ocean", title: "Ocean" },
+                    { value: "People", title: "People" },
+                ],
+                alt: "Boats (Jeshu John - designerspics.com)",
+            },
+
+            {
+                src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+                width: 320,
+                height: 212,
+            },
+        ];
 
         return (
             <div>
-                <button type="button" onClick={() => this.setState({ isOpen: true })}>
+
+
+
+                <Gallery images={images} defaultContainerWidth={1200}/>
+
+{/*                <button type="button" onClick={() => this.setState({ isOpen: true })}>
                     Open Lightbox
                 </button>
 
@@ -44,8 +75,8 @@ export default class ImageLightbox extends Component {
                                 photoIndex: (photoIndex + 1) % images.length,
                             })
                         }
-                    />
-                )}
+                    />}
+                )}*/}
             </div>
         );
     }
