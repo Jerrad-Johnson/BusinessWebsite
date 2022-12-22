68c6a938a46a09d8ae5bb185edc510b8b8d7c5b9
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-22 00:34:37 -0700

    Added package justified-layout

--- frontend/components/Heads.tsx
+++ frontend/components/Heads.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import {businessName} from "../common/variables";
+import fjGallery from "flickr-justified-gallery";
 
 const linkRel = ( <link rel="icon" href="/favicon.ico" /> );
+cc(fjGallery)
 
 export function GenericHead({metaName, content}){
     return (
