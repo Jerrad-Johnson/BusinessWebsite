ef3ef6ea807d10062090481de104e0bdef4ea022
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-12-21 17:43:52 -0700

    Started Gallery page.
    Fixed useScreenOrientation again.

--- frontend/common/variables.ts
+++ frontend/common/variables.ts
@@ -1,3 +1,4 @@
 export const serverUrl = "http://localhost:3001";
 export const mapDefaultLocation: number[] = [44.0805,-103.2310];
 export const businessName: string = "Ideal Portraits";
+export const cc = console.log();
