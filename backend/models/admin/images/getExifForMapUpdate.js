5a1db091f2b8c39a3b50b860723634aa9269453f
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-01-23 01:31:58 -0700

    Refactored getExifForMapUpdate to use local FS instead of localhost.

--- backend/models/admin/images/getExifForMapUpdate.js
+++ backend/models/admin/images/getExifForMapUpdate.js
@@ -4,26 +4,31 @@ const {pathToLeafletThumbnailsForExifReader, cc, errorExistsInScript, ct, pathTo
     pathToLocalhostGallery10pxThumbnailsWithExif
 } = require("../../../common/variables");
 const path = require("path");
+const {readFileSync} = require("fs");
 
 exports.getExifForMapUpdate = async (fileAndFolderNames) => {
-    let largeImg, thumbnailImg, tinyImg;
-    let largeImgsExif = {}, thumbnailImgsExif = {}, tinyImgsExif = {};
+    let largeImg, smallImg, tinyImg;
+    let largeImgsExif = {}, smallImgsExif = {}, tinyImgsExif = {};
+    let largeImgBuffer, smallImgBuffer, tinyImgBuffer;
 
     try {
         for (let folderName in fileAndFolderNames){
             largeImgsExif[folderName] = [];
-            thumbnailImgsExif[folderName] = [];
+            smallImgsExif[folderName] = [];
             tinyImgsExif[folderName] = [];
             for (let fileName of fileAndFolderNames[folderName]){
-                largeImg = await ExifReader.load(path.join(pathToLocalhostGallery1920pxThumbnailsWithExif, folderName, fileName));
+                largeImgBuffer = readFileSync(path.join(pathToLocalhostGallery1920pxThumbnailsWithExif, folderName, fileName))
+                largeImg = await ExifReader.load(largeImgBuffer);
                 largeImg.fileName = fileName;
                 largeImgsExif[folderName].push(largeImg);
 
-                thumbnailImg = await ExifReader.load(path.join(pathToLocalhostGalleryThumbnailsWithExif, folderName, fileName));
-                thumbnailImg.fileName = fileName;
-                thumbnailImgsExif[folderName].push(thumbnailImg);
+                smallImgBuffer = readFileSync(path.join(pathToLocalhostGalleryThumbnailsWithExif, folderName, fileName))
+                smallImg = await ExifReader.load(smallImgBuffer);
+                smallImg.fileName = fileName;
+                smallImgsExif[folderName].push(smallImg);
 
-                tinyImg = await ExifReader.load(path.join(pathToLocalhostGallery10pxThumbnailsWithExif, folderName, fileName));
+                tinyImgBuffer = readFileSync(path.join(pathToLocalhostGallery10pxThumbnailsWithExif, folderName, fileName))
+                tinyImg = await ExifReader.load(tinyImgBuffer);
                 tinyImg.fileName = fileName;
                 tinyImgsExif[folderName].push(tinyImg);
             }
@@ -33,5 +38,5 @@ exports.getExifForMapUpdate = async (fileAndFolderNames) => {
         return errorExistsInScript;
     }
 
-    return {largeImgsExif, thumbnailImgsExif, tinyImgsExif};
+    return {largeImgsExif, thumbnailImgsExif: smallImgsExif, tinyImgsExif};
 }
