a2ac7400b60e682353499fb148eab6b654af7d83
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-01-25 00:36:32 -0700

    added base64url to gallery table data.

--- backend/controllers/leaflet/LeafletPathsControllerGet.js
+++ backend/controllers/leaflet/LeafletPathsControllerGet.js
@@ -1,11 +1,12 @@
 const {getDataForLeaflet} = require("../../models/leaflet/getDataForLeaflet");
 const {standardizedResponse} = require("../../utils/fns");
+const {ct} = require("../../common/variables");
 
 exports.LeafletImagePathsControllerGet = async (req, res, next) => {
     try {
         results = await getDataForLeaflet(req, res, next);
     } catch (e) {
-        cc(e);
+        ct(e);
         res.status(500).send(standardizedResponse("MySQL Error", e))
         return;
     }
