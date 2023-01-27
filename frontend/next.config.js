2d8b6255c35ef7fd209189c1d56204f178aaf379
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-01-27 15:09:04 -0700

    Added remote patterns.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,24 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'https://businessbackend.jerradjohnson.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'businessbackend.jerradjohnson.com',
+        port: '443',
+        pathname: '/galleries/**',
+      },      {
+        protocol: 'https',
+        hostname: 'businessbackend.jerradjohnson.com',
+        port: '80',
+        pathname: '/galleries/**',
+      },      {
+        protocol: 'https',
+        hostname: 'businessbackend.jerradjohnson.com',
+        port: '3002',
+        pathname: '/galleries/**',
+      },
+    ],
   },
   env: {
     SERVERURL: process.env.SERVERURL,
