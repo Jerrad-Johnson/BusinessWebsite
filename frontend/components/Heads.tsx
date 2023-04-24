e54845a2378a00aeaa2b812d1d85f3047bb3b185
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2023-04-24 01:10:07 -0600

    Typescript stuff.

--- frontend/components/Heads.tsx
+++ frontend/components/Heads.tsx
@@ -12,7 +12,7 @@ export function GenericHead({metaName, content}: GenericHeadAttributes){ // TODO
             <meta name={"description"} content={content && content} />
             {linkRel}
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
             <link href="https://fonts.googleapis.com/css2?family=Allura&display=swap" rel="stylesheet"/>
             <link href="https://fonts.googleapis.com/css2?family=Arapey&display=swap" rel="stylesheet"/>
         </Head>
